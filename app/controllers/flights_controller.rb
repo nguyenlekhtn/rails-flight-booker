1fe548983a9afc260b0efc51e70f8300d61a5b6b
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-03-18 22:07:45 +0700

    add all fields to search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @depature_codes = Airport.all.map { |airport| [airport.code, airport.id] }
-    @arrival_airports = @depature_airports
+    @arrival_codes = @depature_codes
     @num_passengers = [1, 2, 3, 4].map { |num| [num, num] }
-    @dates = Flight.order(:start_time).map { |flight| [flight.start_time.strftime("%A, %d %b %Y %l:%M %p"), flight.start_time] }
+    @dates = Flight.order(:start_time).to_a.lazy.map { |flight| flight.start_time.to_date }.to_set.map { |time| [time.strftime("%A, %d %b %Y %l:%M %p"), time.strftime("%Y%m%d")] }
   end
 end

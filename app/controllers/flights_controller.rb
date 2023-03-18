f050346695f2c2db92b5115fc660d99606ebe187
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-03-18 21:19:54 +0700

    add departure code dropdow in index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @depature_airports = Airport.all.map { |airport| [airport.code, airport.id] }
+    @depature_codes = Airport.all.map { |airport| [airport.code, airport.id] }
     @arrival_airports = @depature_airports
     @num_passengers = [1, 2, 3, 4].map { |num| [num, num] }
-    @dates = Flight.order(:start_time).map { |flight| [flight.start_time.strftime("%A, %d %b %Y %l:%M %p"), flight.start_time.] }
+    @dates = Flight.order(:start_time).map { |flight| [flight.start_time.strftime("%A, %d %b %Y %l:%M %p"), flight.start_time] }
+  end
 end

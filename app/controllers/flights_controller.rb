18c346a62b43d010f1de42c253484886d7de5841
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-03-19 22:24:25 +0700

    display search result

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,16 @@ class FlightsController < ApplicationController
     @arrival_codes = @depature_codes
     @num_passengers = [1, 2, 3, 4].map { |num| [num, num] }
     @dates = Flight.order(:start_time).to_a.lazy.map { |flight| flight.start_time.to_date }.to_set.map { |time| [time.strftime("%A, %d %b %Y %l:%M %p"), time.strftime("%Y%m%d")] }
+    if search_query_existed?
+      departure_airport_id = params[:departure_code]
+      arrival_airport_id = params[:arrival_code]
+      start_date = Date.strptime(params[:date], "%Y%m%d")
+      start_time = start_date.beginning_of_day..start_date.end_of_day
+      @matched_flights = Flight.includes(:departure_airport, :arrival_airport).where(departure_airport_id:, arrival_airport_id:, start_time:)
+    end
+  end
+
+  private def search_query_existed?
+    params[:departure_code] && params[:arrival_code] && params[:date] && params[:num_tickets]
   end
 end

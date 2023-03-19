64b40f25f13a19f47b8de6509421f1a1a01317ed
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-03-20 05:29:08 +0700

    make select array not dup if name == id

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
   def index
     @depature_codes = Airport.all.map { |airport| [airport.code, airport.id] }
     @arrival_codes = @depature_codes
-    @num_passengers = [1, 2, 3, 4].map { |num| [num, num] }
+    @num_passengers = [1, 2, 3, 4]
     @dates = Flight.order(:start_time).to_a.lazy.map { |flight| flight.start_time.to_date }.to_set.map { |time| [time.strftime("%A, %d %b %Y %l:%M %p"), time.strftime("%Y%m%d")] }
     if search_query_existed?
       departure_airport_id = params[:departure_code]

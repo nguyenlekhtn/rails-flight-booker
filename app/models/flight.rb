45170e1930ce76a12471f80f669b07d17b5b874b
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-03-21 05:45:55 +0700

    create BookingsController

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings
 
   def details
     "Flight from #{departure_airport.code} to #{arrival_airport.code} at #{start_time.strftime("%A, %d %b %Y %l:%M %p")}"
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,6 @@
 class Passenger < ApplicationRecord
   validates_presence_of :name, :email
-  validates_format_of :email, with: EMAIL_REGEX
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
+  belongs_to :booking
 end

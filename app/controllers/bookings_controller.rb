4ded79e6275a475f18630969337be8c242356ee0
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-03-21 22:16:40 +0700

    use helper association method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,9 @@
 class BookingsController < ApplicationController
   def new
     @flight_id = params[:flight_id]
-    @booking = Booking.new(flight_id: @flight_id)
     @num_tickets = params[:num_tickets]
     @flight = Flight.find(@flight_id)
+    @booking = @flight.bookings.build
     num_tickets = params[:num_tickets].to_i
     num_tickets.times { @booking.passengers.build }
   end

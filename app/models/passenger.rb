f7005a1acb8e70a5b7c64eec1ddcb40650a917e5
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-06-25 11:05:40 +0700

    add length validation for passenger model fields

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,6 @@
 class Passenger < ApplicationRecord
   validates_presence_of :name, :email
+  validates :name, :email, length: {minimum: 3}
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 
   belongs_to :booking

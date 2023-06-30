179d48d83cb68e088a4e4bae13e5bed2f886035e
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-06-30 07:06:50 +0700

    allow to remove passengers by clicking Remove button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,6 +13,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to @booking
     else
+      @flight = @booking.flight
       render :new, status: :unprocessable_entity
     end
   end

464e8684e3d5bb1dff33e1f349a6c227eeab9993
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-07-17 21:53:21 +0700

    fix all failing tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get not_found" do
     get errors_not_found_url
-    assert_response :success
+    assert_response :missing
   end
 
   test "should get internal_server_error" do
     get errors_internal_server_error_url
-    assert_response :success
+    assert_response :error
   end
 end
--- test/mailers/passenger_mailer_test.rb
+++ test/mailers/passenger_mailer_test.rb
@@ -6,13 +6,13 @@ class PassengerMailerTest < ActionMailer::TestCase
   # end
   test "confirmation" do
     passenger = Passenger.new(name: "aaa", email: "mail@example.com")
-    email = PassengerMailer.confirmation_email(passenger)
+    email = PassengerMailer.confirmation_email(passengers(:passenger_one))
 
     assert_emails 1 do
       email.deliver_now
     end
     assert_equal ["ashvsmay00gmail.com"], email.from
-    assert_equal ["mail@example.com"], email.to
+    assert_equal ["one@examkple.com"], email.to
     assert_equal "You have booked your ticket", email.subject
   end
 end
